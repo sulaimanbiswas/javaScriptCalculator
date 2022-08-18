2ec51298b65c62607e67404405a254e28b67732f
Suliaman Biswas <sulaimanbiswasbd@gmail.com>
2022-08-18 15:06:41 +0600

    Complete Project

---
+++ calculator.js
@@ -0,0 +1,22 @@
+let result = document.getElementById("input-text");
+
+let calculate = (number) => {
+  result.value += number;
+
+  console.log(result.value);
+};
+let Result = () => {
+  try {
+    result.value = eval(result.value);
+  } catch (err) {
+    result.value = "Error";
+  }
+};
+
+function clr() {
+  result.value = "";
+}
+
+function del() {
+  result.value = result.value.slice(0, -1);
+}
